c7a8043e45449a31469f5e1df3d137fb8c279de7
sariwahyu <sariwahyuningsih22@gmail.com>
2019-12-30 04:58:36 +0700

    Button Select Car
    membuat UI dan fungsi memilih jenis mobil

---
+++ Assets/CarSelector.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSelector : MonoBehaviour
+{
+    public GameObject lowoireng;
+    public GameObject jazz;
+    public GameObject oldracecar;
+    public GameObject sportcar;
+
+    public int carSelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lowoireng.SetActive(true);
+        jazz.SetActive(false);
+        oldracecar.SetActive(false);
+        sportcar.SetActive(false);
+
+        carSelected = 1;
+    }
+
+    public void loadLowo()
+    {
+        lowoireng.SetActive(true);
+        jazz.SetActive(false);
+        oldracecar.SetActive(false);
+        sportcar.SetActive(false);
+
+        carSelected = 1;
+    }
+
+    public void loadJazz()
+    {
+        lowoireng.SetActive(false);
+        jazz.SetActive(true);
+        oldracecar.SetActive(false);
+        sportcar.SetActive(false);
+
+        carSelected = 1;
+    }
+
+    public void loadOldracecar()
+    {
+        lowoireng.SetActive(false);
+        jazz.SetActive(false);
+        oldracecar.SetActive(true);
+        sportcar.SetActive(false);
+
+        carSelected = 1;
+    }
+
+    public void loadSportcar()
+    {
+        lowoireng.SetActive(false);
+        jazz.SetActive(false);
+        oldracecar.SetActive(false);
+        sportcar.SetActive(true);
+
+        carSelected = 1;
+    }
+}
