6639f197de0177aaeb55197790ba23f6014a0af8
sariwahyu <sariwahyuningsih22@gmail.com>
2019-12-31 22:05:40 +0700

    steer selector
    pilih ganti stir

---
+++ Assets/SteerSelect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteerSelect : MonoBehaviour
+{
+    public GameObject lowoireng;
+    public GameObject steerlain;
+
+    public int steerSelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lowoireng.SetActive(true);
+        steerlain.SetActive(false);
+
+        steerSelected = 1;
+    }
+
+    public void loadLowo()
+    {
+        lowoireng.SetActive(true);
+        steerlain.SetActive(false);
+
+        steerSelected = 1;
+    }
+
+    public void loadsteerlain()
+    {
+        lowoireng.SetActive(false);
+        steerlain.SetActive(true);
+
+        steerSelected = 1;
+    }
+
+}
