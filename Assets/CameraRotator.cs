7c1b95bb60529a40b2c8e97ab2575523c329d258
sariwahyu <sariwahyuningsih22@gmail.com>
2019-12-31 13:40:35 +0700

    scroll wheel zoom

--- Assets/CameraRotator.cs
+++ Assets/CameraRotator.cs
@@ -6,7 +6,6 @@ public class CameraRotator : MonoBehaviour
 {
     public float speed;
 
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, speed * Time.deltaTime, 0);
