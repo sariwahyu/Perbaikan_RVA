6f3c7b16a4a57ee5feb07dc46d5d3ac40522b2a4
sariwahyu <sariwahyuningsih22@gmail.com>
2019-12-31 23:06:56 +0700

    music in, body select

---
+++ Assets/BodySelect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodySelect : MonoBehaviour
+{
+    public GameObject lowoireng;
+    public GameObject bodylain;
+
+    public int bodySelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lowoireng.SetActive(true);
+        bodylain.SetActive(false);
+
+        bodySelected = 1;
+    }
+
+    public void loadLowo()
+    {
+        lowoireng.SetActive(true);
+        bodylain.SetActive(false);
+
+        bodySelected = 1;
+    }
+
+    public void loadbodylain()
+    {
+        lowoireng.SetActive(false);
+        bodylain.SetActive(true);
+
+        bodySelected = 1;
+    }
+}
